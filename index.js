ddbaf2f072ac8c7fc269f7feeb2dde612c1d1e5e
Biplob106 <biplobsd106@gmail.com>
2026-05-18 22:40:58 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const dns = require('dns');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
 
 dotenv.config();
 
@@ -11,10 +15,44 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+// MongoDB URI
+const uri = process.env.MONGODB_URI;
+
+// Create Mongo Client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  await client.connect();
+
+  const database = client.db('sportHavenDB');
+  const facilitiesCollection = database.collection('facilities');
+  const bookingsCollection = database.collection('bookings');
+
+  app.get('/facilities', async (req, res) => {
+    const result = await facilitiesCollection.find().toArray();
+    res.send(result);
+  });
+
+  await client.db('admin').command({ ping: 1 });
+  console.log(' MongoDB Connected Successfully');
+}
+
+run().catch((err) => {
+  console.error('❌ MongoDB connection failed:', err.message);
+});
+
+// Root Route
 app.get('/', (req, res) => {
   res.send('SportHaven Server Running');
 });
 
+// Server Listen
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+  console.log(` Server running on port ${port}`);
 });
